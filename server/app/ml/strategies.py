17f3e7e0a63aa7609970d5f2ae3c8c10b8b4dbc1
womiekom <raiyan.mufidgaming@gmail.com>
2026-08-16 01:50:10 +0700

    feat: define competitive strategy and weather ability mappings

---
+++ server/app/ml/strategies.py
@@ -0,0 +1,29 @@
+
+STRATEGIES = {
+    "rain": {
+        "weather_abilities": ["drizzle"],
+        "weather_abuser_abilities": ["swift-swim"]
+    },
+    "sun": {
+        "weather_abilities": ["drought"],
+        "weather_abuser_abilities": ["chlorophyll", "protosynthesis"]
+    },
+    "sand": {
+        "weather_abilities": ["sand-stream"],
+        "weather_abuser_abilities": ["sand-rush", "sand-force"]
+    },
+    "snow": {
+        "weather_abilities": ["snow-warning"],
+        "weather_abuser_abilities": ["slush-rush"]
+    },
+    "trick_room": {
+        "setters": [
+            "hatterene", "cresselia", "porygon2", "bronzong", 
+            "mimikyu", "indeedee-f", "farigiraf"
+        ],
+        "abusers": [
+            "ursaluna", "amoonguss", "iron-hands", "hariyama", 
+            "slowking", "clodsire"
+        ]
+    }
+}
